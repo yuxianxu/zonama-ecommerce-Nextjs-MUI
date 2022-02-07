fb4be7632a634c82752351706d1e400373f6c5c9
yuxianxu <yuxian.xu@gmail.com>
2022-02-07 09:56:43 -0800

    Add cart and login button to the header

--- components/Layout.js
+++ components/Layout.js
@@ -1,22 +1,40 @@
 import React, { Children } from 'react';
 import Head from 'next/head';
-import { AppBar, Container, Toolbar, Typography } from '@mui/material';
+import NextLink from 'next/link';
+import { AppBar, Container, Toolbar, Typography, Link } from '@mui/material';
 import useStyles from '../utils/styles';
 
-export default function Layout({children}) {
-    const classes = useStyles();
-  return <div>
+export default function Layout({ children }) {
+  const classes = useStyles();
+  return (
+    <div>
       <Head>
         <title>zonama</title>
       </Head>
       <AppBar position="static" className={classes.navbar}>
-          <Toolbar>
-              <Typography>zonama</Typography>
-          </Toolbar>
+        <Toolbar>
+          <NextLink href="/" passHref>
+              <Link>
+            <Typography className={classes.brand}>zonama</Typography>
+              </Link>
+          </NextLink>
+          <div className={classes.grow}></div>
+          <div>
+              <NextLink href="/cart" passHref>
+                  <Link>Cart</Link>
+              </NextLink>
+              <NextLink href="/login" passHref>
+                  <Link>Login</Link>
+              </NextLink>
+          </div>
+        </Toolbar>
       </AppBar>
       <Container className={classes.main}>{children}</Container>
       <footer className={classes.footer}>
-          <Typography>All rights reserved. Yuxian Xu ecommerce shopping store.</Typography>
+        <Typography>
+          All rights reserved. Yuxian Xu ecommerce shopping store.
+        </Typography>
       </footer>
-  </div>;
+    </div>
+  );
 }
