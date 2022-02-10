c364802d7c62852b7c73a289b880b5bcaa1a4375
yuxianxu <yuxian.xu@gmail.com>
2022-02-10 14:52:10 -0800

    Update preview order page with order summary

---
+++ pages/placeorder.js
@@ -0,0 +1,199 @@
+import {
+  Button,
+  Card,
+  Grid,
+  Link,
+  List,
+  ListItem,
+  MenuItem,
+  Select,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from '@mui/material';
+import React, { useContext, useEffect } from 'react';
+import dynamic from 'next/dynamic';
+import axios from 'axios';
+import Layout from '../components/Layout';
+import NextLink from 'next/link';
+import { Store } from '../utils/Store';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import useStyles from '../utils/styles';
+import CheckoutWizard from '../components/CheckoutWizard';
+
+function Placeorder() {
+  const classes = useStyles();
+  const router = useRouter();
+  const { state, dispatch } = useContext(Store);
+  const {
+    cart: { cartItems, shippingAddress, paymentMethod },
+  } = state;
+
+  const round2 = (num) => Math.round(num * 100 + Number.EPSILON) / 100;
+  const itemsPrice = cartItems.reduce((a, c) => a + c.price * c.quantity, 0);
+  const shippingPrice = itemsPrice > 200 ? 0 : 15;
+  const taxPrice = round2(itemsPrice * 0.15);
+  const totalPrice = round2(itemsPrice + taxPrice + shippingPrice);
+
+  useEffect(() => {
+    if (!paymentMethod) {
+      router.push('/payment');
+    }    
+  }, []);
+
+  return (
+    <Layout title="Shopping Cart">
+      <CheckoutWizard activeStep={3}></CheckoutWizard>
+      <Typography component="h1" variant="h1">
+        Place Order
+      </Typography>
+      <Grid container spacing={1}>
+        <Grid item md={9} xs={12}>
+          <Card className={classes.section}>
+            <List>
+              <ListItem>
+                <Typography component="h2" variant="h2">
+                  Shipping Address
+                </Typography>
+              </ListItem>
+              <ListItem>
+                {shippingAddress.fullName}, {shippingAddress.address},{' '}
+                {shippingAddress.city}, {shippingAddress.postalCode},{' '}
+                {shippingAddress.country}
+              </ListItem>
+            </List>
+          </Card>
+          <Card className={classes.section}>
+            <List>
+              <ListItem>
+                <Typography component="h2" variant="h2">
+                  Payment Method
+                </Typography>
+              </ListItem>
+              <ListItem>{paymentMethod}</ListItem>
+            </List>
+          </Card>
+          <Card className={classes.section}>
+            <List>
+              <ListItem>
+                <Typography component="h2" variant="h2">
+                  Order Items
+                </Typography>
+              </ListItem>
+              <ListItem>
+                <TableContainer>
+                  <Table>
+                    <TableHead>
+                      <TableRow>
+                        <TableCell>Image</TableCell>
+                        <TableCell>Name</TableCell>
+                        <TableCell align="right">Quantity</TableCell>
+                        <TableCell align="right">Price</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {cartItems.map((item) => (
+                        <TableRow key={item._id}>
+                          <TableCell>
+                            <NextLink href={`/product/${item.slug}`} passHref>
+                              <Link>
+                                <Image
+                                  src={item.image}
+                                  alt={item.name}
+                                  width={50}
+                                  height={50}
+                                ></Image>
+                              </Link>
+                            </NextLink>
+                          </TableCell>
+                          <TableCell>
+                            <NextLink href={`/product/${item.slug}`} passHref>
+                              <Link>
+                                <Typography>{item.name}</Typography>
+                              </Link>
+                            </NextLink>
+                          </TableCell>
+                          <TableCell align="right">
+                            <Typography>{item.quantity}</Typography>
+                          </TableCell>
+                          <TableCell align="right">
+                            <Typography>$ {item.price}</Typography>
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              </ListItem>
+            </List>
+          </Card>
+        </Grid>
+        <Grid item md={3} xs={12}>
+          <Card className={classes.section}>
+            <List>
+              <ListItem>
+                <Typography variant="h2">Order Summary</Typography>
+              </ListItem>
+              <ListItem>
+                <Grid container>
+                  <Grid item xs={6}>
+                    <Typography>Items:</Typography>
+                  </Grid>
+                </Grid>
+                <Grid item xs={6}>
+                  <Typography align="right">$ {itemsPrice}</Typography>
+                </Grid>
+              </ListItem>
+              <ListItem>
+                <Grid container>
+                  <Grid item xs={6}>
+                    <Typography>Tax:</Typography>
+                  </Grid>
+                </Grid>
+                <Grid item xs={6}>
+                  <Typography align="right">$ {taxPrice}</Typography>
+                </Grid>
+              </ListItem>
+              <ListItem>
+                <Grid container>
+                  <Grid item xs={6}>
+                    <Typography>Shipping:</Typography>
+                  </Grid>
+                </Grid>
+                <Grid item xs={6}>
+                  <Typography align="right">$ {shippingPrice}</Typography>
+                </Grid>
+              </ListItem>
+              <ListItem>
+                <Grid container>
+                  <Grid item xs={6}>
+                    <Typography>
+                      <strong>Total:</strong>
+                    </Typography>
+                  </Grid>
+                </Grid>
+                <Grid item xs={6}>
+                  <Typography align="right">
+                    <strong>$ {totalPrice}</strong>
+                  </Typography>
+                </Grid>
+              </ListItem>
+              <ListItem>
+                <Button variant="contained" color="primary" fullWidth>
+                  Place Order
+                </Button>
+              </ListItem>
+            </List>
+          </Card>
+        </Grid>
+      </Grid>
+    </Layout>
+  );
+}
+
+export default dynamic(() => Promise.resolve(Placeorder), { ssr: false });
