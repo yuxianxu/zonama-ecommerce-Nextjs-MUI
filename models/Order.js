e85d19f974111f25b6022758901db1a1a19cad17
yuxianxu <yuxian.xu@gmail.com>
2022-02-14 22:45:58 -0800

    Fix slug sparse  for orderItems

--- models/Order.js
+++ models/Order.js
@@ -6,7 +6,7 @@ const orderSchema = new mongoose.Schema(
     orderItems: [
       {
         name: { type: String, required: true },
-        slug: { type: String, required: true, sparse: true },
+        slug: { type: String, required: true },
         quantity: { type: Number, required: true },
         image: { type: String, required: true },
         price: { type: Number, required: true },
