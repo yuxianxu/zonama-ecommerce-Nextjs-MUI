02ae7936c6eff2c92da2939ea67a20d338abef98
yuxianxu <yuxian.xu@gmail.com>
2022-02-16 22:06:10 -0800

    Create search page

---
+++ pages/search.js
@@ -0,0 +1,185 @@
+import {
+  Grid,
+  List,
+  ListItem,
+  MenuItem,
+  Select,
+  Typography,
+} from '@mui/material';
+import { Box } from '@mui/system';
+import { useRouter } from 'next/router';
+import React from 'react';
+import Layout from '../components/Layout';
+import Product from '../models/Product';
+import db from '../utils/db';
+import useStyles from '../utils/styles';
+
+const PAGE_SIZE = 3;
+
+export default function Search(props) {
+  const classes = useStyles();
+  const router = useRouter();
+  const {
+    query = 'all',
+    category = 'all',
+    brand = 'all',
+    price = 'all',
+    rating = 'all',
+    sort = 'featured',
+  } = router.query;
+  const { products, countProducts, categories, brands, pages } = props;
+
+  const filterSearch = ({
+    page,
+    category,
+    brand,
+    sort,
+    min,
+    max,
+    searchQuery,
+    price,
+    rating,
+  }) => {
+    const path = router.pathname;
+    const { query } = router;
+    if (page) query.page = page;
+    if (searchQuery) query.searchQuery = searchQuery;
+    if (sort) query.sort = sort;
+    if (category) query.category = category;
+    if (brand) query.brand = brand;
+    if (price) query.price = price;
+    if (rating) query.rating = rating;
+    if (min) query.min ? query.min : query.min === 0 ? 0 : min;
+    if (max) query.max ? query.max : query.max === 0 ? 0 : max;
+
+    router.push({
+      pathname: path,
+      query: query,
+    });
+  };
+
+  const categoryHandler = (e) => {
+    filterSearch({ category: e.target.value });
+  };
+
+  return (
+    <Layout title="Search">
+      <Grid className={classes.mt1} container spacing={1}>
+        <Grid item md={3}>
+          <List>
+            <ListItem>
+              <Box className={classes.fullWidth}>
+                <Typography>Categories</Typography>
+                <Select fullWidth value={category} onChange={categoryHandler}>
+                  <MenuItem value="all">All</MenuItem>
+                  {categories &&
+                    categories.map((category) => (
+                      <MenuItem key={category} value={category}>
+                        {category}
+                      </MenuItem>
+                    ))}
+                </Select>
+              </Box>
+            </ListItem>
+          </List>
+        </Grid>
+      </Grid>
+      <Grid item md={9}></Grid>
+    </Layout>
+  );
+}
+
+export async function getServerSideProps({ query }) {
+  await db.connect();
+  const pageSize = query.pageSize || PAGE_SIZE;
+  const page = query.page || 1;
+  const category = query.category || '';
+  const brand = query.brand || '';
+  const price = query.price || '';
+  const rating = query.rating || '';
+  const sort = query.sort || '';
+  const searchQuery = query || '';
+
+  const queryFilter =
+    searchQuery && searchQuery !== 'all'
+      ? {
+          name: {
+            $regex: searchQuery,
+            $options: 'i',
+          },
+        }
+      : {};
+  const categoryFilter = category && category !== 'all' ? { category } : {};
+  const brandFilter = brand && brand !== 'all' ? { brand } : {};
+  const ratingFilter =
+    rating && rating !== 'all'
+      ? {
+          rating: {
+            $gte: Number(rating),
+          },
+        }
+      : {};
+
+  const priceFilter =
+    price && price !== 'all'
+      ? {
+          price: {
+            $gte: Number(price.split('-')[0]),
+            $lte: Number(price.split('-')[1]),
+          },
+        }
+      : {};
+
+  const order =
+    sort === 'featured'
+      ? { featured: -1 }
+      : sort === 'lowest'
+      ? { price: 1 }
+      : sort === 'highest'
+      ? { price: -1 }
+      : sort === 'toprated'
+      ? { rating: -1 }
+      : sort === 'newest'
+      ? { createdAt: -1 }
+      : { _id: -1 };
+
+  const categories = await Product.find().distinct('category');
+  const brands = await Product.find().distinct('brand');
+  const productDocs = await Product.find(
+    {
+      ...queryFilter,
+      ...categoryFilter,
+      ...priceFilter,
+      ...brandFilter,
+      ...ratingFilter,
+    },
+    '-reviews'
+  )
+    .sort(order)
+    .skip(pageSize * (page - 1))
+    .limit(pageSize)
+    .lean();
+
+  const countProducts = await Product.countDocuments({
+    ...queryFilter,
+    ...categoryFilter,
+    ...priceFilter,
+    ...brandFilter,
+    ...ratingFilter,
+  });
+
+  await db.disconnect();
+
+  const products = productDocs.map(db.convertDocToObj);
+
+  return {
+    props: {
+      products,
+      countProducts,
+      page,
+      pages: Math.ceil(countProducts / pageSize),
+      categories,
+      brands,
+    },
+  };
+}
